49b4d0476e9b612b742d0d3a3db59235b8fc5349
nielsdrost7 <nielsdrost7@gmail.com>
2024-11-13 19:52:53 +0100

    changes

--- phpmeup.sh
+++ phpmeup.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-# shellcheck disable=SC2046
 docker exec --env-file=.env.docker -it --user=ivpldock $(docker ps -aqf "name=php-fpm") bash
--- rootmeup.sh
+++ rootmeup.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-# shellcheck disable=SC2046
 docker exec --env-file .env.docker -it $(docker ps -aqf "name=workspace") bash
